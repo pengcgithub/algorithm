33c98f1250ee7e3ed81a29576ebd6aef8ee12dd6
pengcheng <pengcheng3211@gmail.com>
2019-08-28 19:28:48 +0800

    feat: 增加算法

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # algorithm
 
 - 1、给你一个字符串，你要判断它是否是回文字符串。字符串里只考虑字母和数字，其它的字符可以无视。另外，对于字母，可以忽略大小写。
-[#codeing](./src/main/java/com/algorithm/code/ValidPalindrome.java)
+[#codeing](./src/main/java/com/algorithm/code/One.java)
+
+- 2、给你一个整数数组和一个目标值，你要找到数组里两个整数， 它们的和等于目标值。然后返回这两个整数的下标。
+[#codeing](./src/main/java/com/algorithm/code/Two.java)
+
+- 3、给你一个整数数组，并且这个数组是按递增排序的，你要找到数组中的两个整数，它们的和等于给定的目标值，然后返回它们的下标。题目假设给你的数组总是有且只有一个解，而且同一个元素不能使用两次。另外，返回结果的下标要从 1 开始。
+[#codeing](./src/main/java/com/algorithm/code/Three.java)
---
+++ src/main/java/com/algorithm/code/Three.java
@@ -0,0 +1,46 @@
+package com.algorithm.code;
+
+/**
+ * 给你一个整数数组，并且这个数组是按递增排序的，你要找到数组中的两个整数，它们的和等于给定的目标值，然后返回它们的下标。题目假设给你的数组总是有且只有一个解，而且同一个元素不能使用两次。另外，返回结果的下标要从 1 开始。<br/>
+ *
+ * 比如说给你的数组是：
+ *
+ * 1, 2, 3, 6, 8, 11
+ *
+ * 目标值是 10。那么，满足条件的两个整数是，2 和 8，它们的和是 10。所以你要返回它们的下标是 [2, 5]。
+ *
+ * leetcode 167
+ * lintcode 608
+ *
+ * @author pengc
+ * @version v3.1.0
+ */
+public class Three {
+
+    public int[] getTwoNumSumToGivenValue(int[] nums, int target) {
+
+        int i = 0, j = nums.length - 1;
+
+        while (i < j) {
+            if (nums[i] + nums[j] > target) {
+                --j;
+            } else if (nums[i] + nums[j] < target) {
+                ++i;
+            } else {
+                return new int[]{i + 1, j + 1};
+            }
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String args[]) {
+        Three three = new Three();
+        int[] nums = new int[]{1, 2, 3, 6, 8, 11};
+        int[] result = three.getTwoNumSumToGivenValue(nums, 10);
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+
+}
